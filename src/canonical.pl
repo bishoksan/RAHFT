1ec50dbeaca5b780f8b5b2da5afae2d8127f820b
Jose F. Morales <jfmcjf@gmail.com>
2016-02-01 17:57:26 +0100

    Documenting canonical.pl

--- src/canonical.pl
+++ src/canonical.pl
@@ -3,19 +3,29 @@
 	 canonical_each/1,
 	 melt/2, 
 	 melteach/2,
-	 variable/1], []).
+	 variable/1], [assertions, isomodes, doccomments]).
+
+%! @title Ground representation of terms
 
 :- use_module(library(write), [numbervars/3]).
 
+%! canonical(?T): instantiate `T` variables to obtain its ground
+%    representation (destructively).
 canonical(T) :-
 	numbervars(T,0,_).
-	
+
+%! canonical_each(?Xs): @pred{canonical/1} on each element of `Xs`
 canonical_each([]).
 canonical_each([X|Xs]) :-
 	canonical(X),
 	canonical_each(Xs).
-	
 
+%! melt(+X,-Y): Obtain the free variable term representation `Y` from
+%    ground representation `X`. E.g.,
+%
+%        ?- melt(a('$VAR'(1),'$VAR'(2),'$VAR'(1)),X).
+%       
+%        X = a(_A,_,_A) ? 
 melt(X,Y) :-
 	melt1(X,Y,_Assoclist),
 	!.
@@ -48,8 +58,10 @@ assoc(X,Y,[assoc(X,Y)|_]) :-
 assoc(X,Y,[_|S]) :-
 	assoc(X,Y,S).
 
+%! variable(+X): `X` is a variable in ground representation
 variable('$VAR'(_)).	
 
+%! melteach(+Xs,-Ys): apply `melt/2` for each element
 melteach([],[]).
 melteach([X|Xs],[Y|Ys]) :-
 	melt(X,Y),
