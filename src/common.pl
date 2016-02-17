2f146ae7e0470a1c85fe3e7828b06fdf7f3b8919
bishoksan <bishoksan@gmail.com>
2016-02-17 10:02:22 +0100

    updated common.pl

--- src/common.pl
+++ src/common.pl
@@ -25,7 +25,7 @@ listofList2Disj([A|R], ((A1);R1)):-
     !,
     list2Conj(A, A1),
     listofList2Disj(R, R1).
-listofList2Disj([[]], (1=0)).
+listofList2Disj([], (1=0)).
 
 convert2num(A,A) :-
 	number(A),
