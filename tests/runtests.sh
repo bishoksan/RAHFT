90467a7261d3ee48f8c07476e331a59719fa4764
Jose F. Morales <jfmcjf@gmail.com>
2016-02-20 20:54:08 +0100

    Adding a test script (tests/runtests.sh)
    It automatically compares the output with a saved result.txt-good
    file. If something differs it warns about it.

---
+++ tests/runtests.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# TODO: Does not have timeouts (do not use gtimeout, do it in Ciao)
+# TODO: Needs a value for CIAOPATH
+
+# Exit immediately if a simple command exits with a non-zero status
+set -e
+
+# Physical directory where the script is located
+_base=$(e=$0;while test -L "$e";do d=$(dirname "$e");e=$(readlink "$e");\
+        cd "$d";done;cd "$(dirname "$e")";pwd -P)
+
+set -e
+
+solver=$CIAOPATH/build/bin/rahft
+
+testdir="../examples"
+tests="\
+addition.nts.pl \
+bfprt.nts.pl \
+binarysearch.nts.pl \
+countZero.nts.pl \
+fib.pl \
+identity.nts.pl \
+merge.nts.pl \
+palindrome.nts.pl \
+parity.nts.pl \
+remainder.nts.pl \
+running.nts.pl"
+
+results="result.txt"
+
+cd "$_base"
+
+rm -f "$results"
+for i in $tests; do
+    $solver "$testdir/$i"
+done
+
+if diff <(sed 's/, Time:.*/}/g' "$results") \
+	<(sed 's/, Time:.*/}/g' "$results"-good); then
+    printf "\nTESTS SEEMS OK\n"
+else
+    printf "\nTESTS DIFFER, SOMETHING MAY BE WRONG\n"
+fi
