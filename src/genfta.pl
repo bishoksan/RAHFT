dbb9908ebfa5f482335675ceca545c149a2cd556
bishoksan <bishoksan@gmail.com>
2016-04-30 16:58:29 +0200

    documented genfta.pl

--- src/genfta.pl
+++ src/genfta.pl
@@ -1,6 +1,13 @@
 :- module(genfta, [main/1], []).
 
-% Generate an FTA from a program, and an error trace.
+/*
+Input:  a program P and an error trace t passed in a file
+Output: finite tree automata FTA
+
+It generates finite tree automata of P and finite tree automata of t  following the definition 7 and 11 of http://akira.ruc.dk/~kafle/publications/comlan-15 (the states of these two automata are disjoint). Then it returns the union of them.
+
+*/
+% Generate an FTA from a program P, and an error trace.
 % The error trace is passed in a file as an argument.
 
 :- use_module(library(write)).
